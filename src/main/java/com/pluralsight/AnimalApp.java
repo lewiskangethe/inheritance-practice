a1dd3386c74b4cdf3c775249ccafc32643063db7
lewiskangethe <lewiskkangethe@gmail.com>
2025-10-29 13:06:45 -0400

    second commit - polymorphism

--- src/main/java/com/pluralsight/AnimalApp.java
+++ src/main/java/com/pluralsight/AnimalApp.java
@@ -1,24 +1,38 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class AnimalApp {
     public static void main(String[] args) {
         Animal animal = new Animal();
-        animal.makeSound();
-
 
         Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(cat);
+
+        for (Animal animal1: animals){
+            animal1.makeSound();
+        }
+
+
+
+
+
 //        dog.eat();
 //        dog.sleep();
 //        dog.breathe();
 //        dog.sniffButt();
-        dog.makeSound();
+//        dog.makeSound();
 
-        Animal cat = new Cat();
+//        Animal cat = new Cat();
 //        cat.eat();
 //        cat.sleep();
 //        cat.breathe();
 //        String output = cat.coughHairball();
 //        System.out.println(output);
-        cat.makeSound();
+//        cat.makeSound();
     }
 }
